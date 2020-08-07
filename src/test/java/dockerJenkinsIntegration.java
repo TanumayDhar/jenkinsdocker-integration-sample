c1bc6975770d5332b635e28a4ab9c33efc766469
Tanumay Dhar <tanna.dhar@gmail.com>
2020-08-07 13:34:16 +0530

    3rd

--- src/test/java/dockerJenkinsIntegration.java
+++ src/test/java/dockerJenkinsIntegration.java
@@ -3,7 +3,7 @@ public class dockerJenkinsIntegration {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("This is dockerJenkinsIntegration");
+System.out.println("This is dockerJenkinsIntegration - sample project ");
 	}
 
 }
